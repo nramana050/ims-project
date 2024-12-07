e4dcf48019dc63023819057e5cceb7cb48d009f8
nramana050 <110442371+nramana050@users.noreply.github.com>
2024-12-07 23:02:38 +0530

    Delete Inventory-backend directory

--- Inventory-backend/buyerSupplierModule/buyerSupplierApp.js
+++
@@ -1,8 +0,0 @@
-const app = require("../server");
-
-const { createBuyerSupplier, getBuyersSuppliers } = require("./dataController");
-
-app.post("/buyers-suppliers", createBuyerSupplier);
-app.get("/buyers-suppliers", getBuyersSuppliers);
-
-module.exports = app;
